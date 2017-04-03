288c7e28da9bed91255d70381768ef48a4194e6b
Martin Bidlingmaier <martin.bidlingmaier@gmail.com>
2017-04-03 21:04:30 +0200

    Remove reload feature

--- src/App.purs
+++ src/App.purs
@@ -5,37 +5,29 @@ import Halogen.HTML as HH
 import Raw as Raw
 import Control.Monad.Aff (Aff)
 import Control.Monad.Aff.Class (liftAff)
-import Control.Monad.Aff.Wait (wait)
 import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE)
-import Control.Monad.Eff.Console (log)
-import Control.Monad.Eff.Timer (TIMER)
-import Control.Monad.Rec.Loops (whileM_)
+import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.State.Class (gets, modify)
-import DOM.HTML (window)
-import DOM.HTML.Types (htmlDocumentToDocument)
-import DOM.HTML.Window (document)
-import DOM.Node.Document (url)
+import Data.Array (last, null, snoc, tail)
 import Data.Either (Either(..))
-import Data.Maybe (Maybe(..), fromMaybe, isNothing)
-import Data.Time.Duration (Seconds(..))
+import Data.Maybe (Maybe(..), fromJust)
+import Data.String (Pattern(..), split)
 import GithubMarkdown (GithubApiError, markdownToHtml)
 import Halogen (Component, ParentDSL, ParentHTML, lifecycleParentComponent)
 import Halogen.Aff (awaitBody)
 import Halogen.Aff.Effects (HalogenEffects)
-import Halogen.HTML (ClassName(..), HTML(..), div, slot, text)
+import Halogen.HTML (ClassName(..), HTML, br_, slot, text)
 import Halogen.HTML.Properties (class_)
 import Halogen.VDom.Driver (runUI)
 import Inner (getInnerText, setInnerHTML)
-import Network.HTTP.Affjax (AJAX, affjax, defaultRequest)
-import Network.HTTP.StatusCode (StatusCode(..))
+import Network.HTTP.Affjax (AJAX)
+import Partial.Unsafe (unsafePartial)
 import Raw (rawHTML)
 import Settings (Settings, SettingsOverride, emptyOverride, override)
 
 type Effects e =
   ( console :: CONSOLE
   , ajax :: AJAX
-  , timer :: TIMER
   | HalogenEffects e
   )
 
@@ -47,8 +39,6 @@ type State =
   , globalSettings :: Settings
   , localSettings :: SettingsOverride
   , renderError :: Maybe GithubApiError
-  , reloadError :: Maybe Unit
-  , isAlive :: Boolean
   }
 
 settings :: State -> Settings
@@ -58,10 +48,7 @@ type Output = Void
 type Input = Settings
 
 data Query a = UpdateGlobalSettings a Settings
-             | Reload a
              | Rerender a
-             | Initialize a
-             | Finalize a
 
 data Slot = UnrenderedSlot
           | RenderedSlot
@@ -77,8 +64,6 @@ initialState markdown settings' =
   , globalSettings: settings'
   , localSettings: emptyOverride
   , renderError: Nothing
-  , reloadError: Nothing
-  , isAlive: true
   }
 
 eval :: forall e. Query ~> DSL e
@@ -97,33 +82,22 @@ eval =
         Right html -> do
           modify \s -> s { renderError = Nothing, html = Just html }
       pure next
-    Reload next -> do
-      url <- liftEff $ window >>= (document >>> map htmlDocumentToDocument) >>= url
-      resp <- (liftAff $ affjax $ defaultRequest { url = url })
-      if resp.status /= StatusCode 200 && resp.status /= StatusCode 0
-        then modify \s -> s { reloadError = Just unit } -- todo
-        else do
-          liftEff $ log "Reloaded"
-          let markdown = resp.response
-          oldMarkdown <- gets \s -> s.markdown
-          oldHtml <- gets \s -> s.html
-          modify \s -> s { reloadError = Nothing, markdown = resp.response }
-          when (oldMarkdown /= markdown || isNothing oldHtml) $ eval $ Rerender unit
-      pure next
-    Initialize next -> do
-      eval $ Rerender unit
-      eval $ Reload unit
-      whileM_ (gets _.isAlive) do
-        wait $ Seconds 0.2
-        eval $ Reload unit
-      pure next
-    Finalize next -> do
-      modify \s -> s { isAlive = false }
-      pure next
+
+intersperse :: forall a. a -> Array a -> Array a
+intersperse x xs =
+  if null xs
+  then xs
+  else 
+    unsafePartial $ snoc
+    (fromJust (tail xs) >>= \x' -> [x', x])
+    (fromJust (last xs))
+
+multiline :: forall p i. String -> Array (HTML p i)
+multiline = split (Pattern "\n") >>> map text >>> intersperse br_
 
 render :: forall e. State -> ParentHTML Query Raw.Query Slot (M e)
 render s = case s.html of
-  Nothing -> text s.markdown
+  Nothing -> HH.div_ $ multiline s.markdown
   Just html ->
     HH.div [ class_ $ ClassName "markdown-body" ]
     [ slot RenderedSlot rawHTML html absurd ]
@@ -132,9 +106,9 @@ receiver :: Input -> Maybe (Query Unit)
 receiver = UpdateGlobalSettings unit >>> Just
 
 initializer :: Maybe (Query Unit)
-initializer = Just $ Initialize unit
+initializer = Just $ Rerender unit
 finalizer :: Maybe (Query Unit)
-finalizer = Just $ Finalize unit
+finalizer = Nothing
 
 app :: forall e. String -> Component HTML Query Input Output (M e)
 app initialMarkdown = 
